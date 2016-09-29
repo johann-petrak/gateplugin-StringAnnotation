f98481806548e2162a4855e2372ea4fd46045f37
Johann Petrak <johann.petrak@gmail.com>
2016-09-29 19:04:28 +0100

    Update license and license headers.

--- src/com/jpetrak/gate/stringannotation/regexp/JavaRegexpAnnotator.java
+++ src/com/jpetrak/gate/stringannotation/regexp/JavaRegexpAnnotator.java
@@ -1,4 +1,24 @@
 /*
+ * Copyright (c) 2010- Austrian Research Institute for Artificial Intelligence (OFAI). 
+ * Copyright (C) 2014-2016 The University of Sheffield.
+ *
+ * This file is part of gateplugin-ModularPipelines
+ * (see https://github.com/johann-petrak/gateplugin-ModularPipelines)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
  *  JavaRegexpAnnotator.java
  *
  *  $Id: JavaRegexpAnnotator.java  $
