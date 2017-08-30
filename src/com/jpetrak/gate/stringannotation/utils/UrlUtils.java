bd43f88cc345fc7428af9552abee5cbeec0080c5
Johann Petrak <johann.petrak@gmail.com>
2017-08-30 22:31:14 +0200

    Change implementation of UrlUtils to use the URI and URL classes better.

--- src/com/jpetrak/gate/stringannotation/utils/UrlUtils.java
+++ src/com/jpetrak/gate/stringannotation/utils/UrlUtils.java
@@ -37,7 +37,7 @@ public class UrlUtils {
   
   /**
    * Create a URL from the String.
-   * If the String does not have a protocol/scheme, file:// is prepended.
+   * If the String does not have a protocol/scheme, file:// is assumed and prepended.
    * @param str
    * @return 
    */
@@ -57,23 +57,37 @@ public class UrlUtils {
    * 
    * This tries to roughly provide the equivalent of "new File(dirFile, nameString)"
    * for a URL and a name String. The catch is that in order for this to work,
-   * the given dir URL must end in a slash. This method adds the slash if necessary
-   * and returns the URL of the combined paths.
-   * 
+   * the given dir URL must end in a slash. This method assumes that the first
+   * URL always refers to a directory and therefore appends a slash if necessary.
+   * <p>
+   * Note: if the dirURL contains a query and/or a fragment, those parts are 
+   * lost in the resulting URL.
    * @param dir
    * @param file 
    */
   public static URL newURL(URL dirURL, String fileName) {
-    String s = dirURL.toExternalForm();
-    if(!s.endsWith("/")) {
-      try {
-        dirURL = new URL(s+"/");
-      } catch (MalformedURLException ex) {
-        throw new GateRuntimeException("Could not create URL for "+s+"/",ex);
-      }
+    URI dirURI;
+    try {
+      dirURI = dirURL.toURI();
+    } catch (URISyntaxException ex) {
+      throw new GateRuntimeException("Cannot convert URL to URI: "+dirURL);
+    }
+    String path = dirURI.getPath();
+    if(!path.endsWith("/")) path = path+"/";
+    try {
+      dirURI = new URI(
+              dirURI.getScheme(),
+              dirURI.getUserInfo(),
+              dirURI.getHost(),
+              dirURI.getPort(),
+              path,
+              dirURI.getQuery(),
+              dirURI.getFragment());
+    } catch (URISyntaxException ex) {
+      throw new GateRuntimeException("Cannot conver URL to URI: "+dirURL);
     }
     try {
-      URL ret = new URL(dirURL,fileName);
+      URL ret = new URL(dirURI.toURL(),fileName);
       return ret;
     } catch (MalformedURLException ex) {
       throw new GateRuntimeException("Could not create URL for "+dirURL+"fileName");
@@ -82,63 +96,57 @@ public class UrlUtils {
   
   /**
    * Return the last path component of a hierarchical path of URL.
-   * This assumes a URL which ends in a hierarchical path, without a query 
-   * or anchor or anything else following the path.
-   * If there is nothing that looks like it could be the last path component,
-   * the empty string is returned.
-   * NOTE: if the URL ends with a slash, then the last component without a trailing
-   * slash is returned.
-   * <p>
-   * This is made intentionally to work similar to the someFile.getName() method.
    * @param url
    * @return 
    */
   public static String getName(URL url) {
-    String p;
+    URI uri;
     try {
-      p = url.toURI().getPath();
+      uri = url.toURI();
     } catch (URISyntaxException ex) {
-      throw new GateRuntimeException("Cannot convert URL to URI: "+url,ex);
+      throw new GateRuntimeException("Cannot convert URL to URI: "+url);
     }
-    // remove any trailing slash
-    if(p.endsWith("/")) {
-      p=p.substring(0,p.length()-1);
+    URL parentURL;
+    try {
+      parentURL = new URL(url,".");
+    } catch (MalformedURLException ex) {
+      throw new GateRuntimeException("Cannot find parent URL for URL "+url);
     }
-    int i = p.lastIndexOf("/");
-    if(i<0) return "";  // no slash at all, we do not have a path 
-    return p.substring(i+1);
+    URI parentURI = null;
+    try {
+      parentURI = parentURL.toURI();
+    } catch (URISyntaxException ex) {
+      throw new GateRuntimeException("Cannot convert URL to URI: "+parentURL);
+    }
+    URI relative = parentURI.relativize(uri);
+    return relative.toString();
   }
   
   /**
    * Return the parent path for a URL.
    * This is the path, with the last component of the path removed, i.e.
    * with that part removed that is returned by the getName() method.
-   * <p>
-   * This returns null if the path is a slash or something without a slash.
+   * 
+   */
+  public static URL getParentURL(URL url) {
+    URL ret;
+    try {
+      ret = new URL(url,".");
+    } catch (MalformedURLException ex) {
+      throw new GateRuntimeException("Cannot convert URL to URI: "+url);
+    }
+    return ret;
+  }
+  
+  /**
+   * Return the parent path for a URL.
+   * This is the path, with the last component of the path removed, i.e.
+   * with that part removed that is returned by the getName() method.
    * 
    */
   public static String getParent(URL url) {
-    String p;
-    try {
-      p = url.toURI().getPath();
-    } catch (URISyntaxException ex) {
-      throw new GateRuntimeException("Cannot convert URL to URI: "+url,ex);
-    }
-    if(p.equals("/")) return null;
-    // NOTE: we ignore odd cases where the path is just several slashes in succession
-    
-    // .. not just a lone slash, remove any trailing slash
-    if(p.endsWith("/")) {
-      p=p.substring(0,p.length()-1);
-    }    
-    int i = p.lastIndexOf("/");
-    if(i<0) return null;  // no slash at all
-    
-    // we found the slash that separates the last name part from the rest
-    // the parent is whatever comes before that
-    return p.substring(0,i);    
+    return getParentURL(url).toString();
   }
-  
   
   /** 
    * Returns true if the URL can be opened for reading.
